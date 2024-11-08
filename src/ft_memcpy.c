46342a2d1dbbd6b7d6c5a40cd46225a94162b6e1
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-11-08 16:03:16 +0200

    Update ft_memcpy.c

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -18,3 +18,15 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
 
     return dst;
 }
+
+/*
+unsigned char *tempd = (unsigned char *)dst;
+
+This assigns the address stored in dst (after casting it) to tempd. 
+It tells the compiler that tempd is a pointer to unsigned char, so 
+you can dereference it to read or write a single byte at a time.
+*ptr = (unsigned char *)str; (Incorrect)
+
+This statement means you are trying to assign an address 
+(unsigned char *)str to the value pointed to by ptr, which is not 
+valid because *ptr expects a value, not an address.*/
