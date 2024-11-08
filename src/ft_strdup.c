8b212efb202eec0741f8d6d760ce15115909a3fd
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-11-08 20:42:41 +0200

    Update ft_strdup.c

--- src/ft_strdup.c
+++ src/ft_strdup.c
@@ -1,4 +1,19 @@
+/*
+STRDUP(3) (simplified)
+
+NAME
+    strdup -- save a copy of a string
+SYNOPSIS
+    char *strdup(const char *s1);
+DESCRIPTION
+    The strdup() function allocates sufficient memory for a copy
+    of the string s1, does the copy, and returns a pointer to it.
+    The pointer may subsequently be used as an argument to the
+    function free(3).
+    If insufficient memory is available, NULL is returned and
+    errno is set to ENOMEM.*/
 #include "libft.h"
+#include <stdlib.h>
 
 char ft_strdup(const char *src)
 {
@@ -6,18 +21,18 @@ char ft_strdup(const char *src)
     int n = 0;
     char *dest;
 
-    while (src[n] != '\0')
-    {
-        n++;
-    }
+    n = ft_strlen(src);
 
-    dest = malloc((n+1) * sizeof(char)); //n+1 for null terminator
+    dest = malloc((n + 1) * sizeof(char)); // n+1 for null terminator
     if (dest == NULL)
     {
         return NULL;
     }
 
-    ft_strcpy(dest, src);
+    for (int i = 0; i <= n; i++)
+    {
+        dest[i] = src[i]; // Copy each character including the null terminator
+    }
 
     return dest;
 }
