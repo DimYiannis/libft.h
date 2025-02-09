07d6e7d1bc06a6ae15d820199b713b74391b1676
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-02-09 20:07:44 +0200

    Update libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -6,16 +6,18 @@
 #include <unistd.h>
 
 // Part 1: Function prototypes (C standard library functions reimplemented)
-size_t  ft_strlen(const char *s);
-char    *ft_strcpy(char *dst, const char *src);
-int     ft_strcmp(const char *s1, const char *s2);
-char    *ft_strdup(const char *s1);
-void    *ft_memset(void *b, int c, size_t len);
-void    ft_bzero(void *s, size_t n);
-void    *ft_memcpy(void *dst, const void *src, size_t n);
-void    *ft_memmove(void *dst, const void *src, size_t len);
-void    *ft_memchr(const void *s, int c, size_t n);
-int     ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t ft_strlen(const char *s);
+char *ft_strcpy(char *dst, const char *src);
+int ft_strcmp(const char *s1, const char *s2);
+char *ft_strdup(const char *s1);
+void *ft_memset(void *b, int c, size_t len);
+void ft_bzero(void *s, size_t n);
+void *ft_memcpy(void *dst, const void *src, size_t n);
+void *ft_memmove(void *dst, const void *src, size_t len);
+void *ft_memchr(const void *s, int c, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+int ft_isalnum(int c);
+int ft_isdigit(int c);
 
 // More functions as you implement them...
 
